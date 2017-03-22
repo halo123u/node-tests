425cc332dbee03358fe822448cb3fc28c91e8d8e
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-21 20:43:41 -0400

    adds test cases

---
+++ utils/utils.test.js
@@ -0,0 +1,16 @@
+const utils = require('./utils');
+
+it('should add two numbers', () => {
+    var res = utils.add(33,11);
+
+    if (res !==44){
+        throw new Error(`Expected 44, but got ${res}`);
+    }
+});
+
+it('should square one number', () =>{
+    var res = utils.square(10);
+    if(res !== 100){
+        throw new Error(`Expected 100, but got ${res}`);
+    }
+});
